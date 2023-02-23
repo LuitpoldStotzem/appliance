4806056b00c1405fc566f4caf7bb058a17b8bd33
lst <lst@core4.core4>
2023-02-23 09:06:05 +0100

    chagned bootstrap script to manually set user as env variable

--- bootstrap-bi.sh
+++ bootstrap-bi.sh
@@ -4,8 +4,9 @@
 # bash bootstrap-bi.sh
 
 if (( $EUID != 0 )); then
+    username = $(whoami)
     echo "restarting as root, please enter your password ..."
-    su - -c "export AUTO=1; cd $PWD; /bin/bash $0 $*"
+    su - -c "export AUTO=1; export SUDO_USER=$username; cd $PWD; /bin/bash $0 $*"
     exit
 fi
 
@@ -15,9 +16,9 @@ if [[ $AUTO -ne 1 ]]; then
 fi
 
 if [ -n "$SUDO_USER" ]; then
-    USER="${SUDO_USER:-$(whoami)}"
+    USER="$SUDO_USER"
 else
-    USER="${SUDO_USER:-$USER}"
+    USER="$USER"
 fi
 USERHOME="/home/$USER"
 
